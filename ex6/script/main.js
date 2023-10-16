2f936161c320b141c16b3154b07eee8fb48cb7a4
ZetMGC <leontevdan798@gmail.com>
2023-10-16 22:25:03 +0300

    ex 6 completed

--- ex6/script/main.js
+++ ex6/script/main.js
@@ -1,20 +1,47 @@
 const Prices = [100, 43, 544, 12];
 
 const form = document.getElementById('form');
-form.addEventListener('submit', getFormValue);
+const radio = document.getElementById('radio');
+const HDD = document.getElementById('HDD');
+const CPU = document.getElementById('CPU');
+const SSD = document.getElementById('SSD');
+
+FormChangeHandler();
+
+form.addEventListener('change', getFormValue);
+radio.addEventListener('change', FormChangeHandler);
+
+function FormChangeHandler(event) {
+    if (HDD.checked) {
+        document.getElementById('Select').hidden = true;
+        document.getElementById('checkbox').hidden = true;
+    } else if (CPU.checked) {
+        document.getElementById('Select').hidden = false;
+        document.getElementById('checkbox').hidden = true;
+    } else if (SSD.checked) {
+        document.getElementById('Select').hidden = true;
+        document.getElementById('checkbox').hidden = false;
+    }
+};
 
 function getFormValue(event) {
     event.preventDefault();
     const Number = form.querySelector('[name ="number"]');
-    const Product = form.querySelector('[name ="product"]');
+    const ProductType = form.querySelector('[name ="productType"]');
     const Result = form.querySelector('[name ="result"]');
+    const CheckBox = form.querySelector('[name ="checkbox"]');
     
-    var CurrentCost;
-    if (Product.value === 'SSD') CurrentCost = Prices[0];
-    else if (Product.value === 'HDD') CurrentCost = Prices[1];
-    else if (Product.value === 'CPU') CurrentCost = Prices[2];
-    else if (Product.value === 'RAM') CurrentCost = Prices[3];
-    else alert("Вы не выбрали продукт");
+    if (HDD.checked) {
+        Result.value = (Number.value * 30) + "$";
+    } else if (CPU.checked) {
+        if (ProductType.value === 'new') Result.value = (500 * Number.value * 1.5) + "$";
+        else if (ProductType.value === 'used') Result.value = (500 * Number.value * 0.9) + "$";
+        else if (ProductType.value === 'showcase') Result.value = (500 * Number.value * 1.2) + "$";
+        else Result.value = 'Выберите тип';
+    } else if (SSD.checked) {
+        if (CheckBox.checked) Result.value = (Number.value * 80 + 20) + "$";
+        else Result.value = (Number.value * 80) + "$";
+    }
 
     Result.value = (Number.value * CurrentCost) + "$";
 };
