068eb530bbe262c7cfe9fc870939ad55ef9ecaa5
ZetMGC <leontevdan798@gmail.com>
2023-10-19 12:24:03 +0300

    ex7_slick_

---
+++ ex7_slick/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.img').slick({slidesToShow: 3, 
+        slidesToScroll: 1, 
+        arrows: true,
+        dots: true, 
+        centerMode: true,
+        centerPadding: '0px'
+    });
+});
