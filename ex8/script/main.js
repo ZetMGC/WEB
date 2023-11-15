27e6e62f64eb58e53ee7dc393bf04d2915d34bf4
ZetMGC <leontevdan798@gmail.com>
2023-11-15 19:41:05 +0300

    complete

--- ex8/script/main.js
+++ ex8/script/main.js
@@ -1,30 +1,56 @@
-var slapform = new Slapform();
-const form = document.getElementById('form');
+document.addEventListener("DOMContentLoaded", function () {
+    window.location.href= '#';
 
-const form_inf = {
-    name: document.getElementById("InputName").value,
-    email: document.getElementById("InputEmail").value,
-    phone: document.getElementById("InputTel").value,
-    organization: document.getElementById("InputOrg").value,
-    message: document.getElementById("InputMessage").value,
-    agree: document.getElementById("Check").checked,
-};
+    var Modal = new bootstrap.Modal(document.getElementById('Modal'))
+    const form = document.getElementById('form');
+    const open = document.getElementById('btn-open');
+    const close = document.getElementById('btn-close');
+    const x = document.getElementById('btn-x');
 
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    slapform.submit({
-        form: '4BGY8YmjYI', 
-        data: form_inf
-    })
-    .then(function (response) { // This function runs only on success
-        console.log('Success!', response);
-    })
-    .catch(function (response) { // This function runs only on error
-        console.log('Fail!', response);
-    })
-    .finally(function () { // This function runs regardless of success or error
-        console.log('This always runs!');
+    const form_inf = {
+        name: document.getElementById("InputName").value,
+        email: document.getElementById("InputEmail").value,
+        phone: document.getElementById("InputTel").value,
+        organization: document.getElementById("InputOrg").value,
+        message: document.getElementById("InputMessage").value,
+        agree: document.getElementById("Check").checked,
+    };
+
+    close.addEventListener("click", function(event) { 
+        history.back();
     });
 
-    localStorage.setItem("form_inf", JSON.stringify(form_inf)); //
+    x.addEventListener("click", function(event) {
+        history.back();
+    });
+
+    open.addEventListener("click", function(event) { 
+        history.pushState({ page: "popup" }, "Форма", "#popup"); 
+    });
+
+    form.addEventListener("submit", function (event) {
+        var slapform = new Slapform();
+        event.preventDefault();
+        slapform.submit({
+            form: '4BGY8YmjYI', 
+            data: form_inf,
+        })
+        .then(function (response) { // This function runs only on success
+            console.log('Success!', response);
+        })
+        .catch(function (response) { // This function runs only on error
+            console.log('Fail!', response);
+        })
+        .finally(function () { // This function runs regardless of success or error
+            console.log('This always runs!');
+        });
+
+        localStorage.setItem("form_inf", JSON.stringify(form_inf));
+        setTimeout(function () {
+            history.back();
+            Modal.hide();
+            form.reset();
+            localStorage.removeItem("form_inf");
+        }, 1000);
+    });
 });
